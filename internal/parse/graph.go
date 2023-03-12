eeaa4df7815138ca8908609bbe960c300968e78e
haokai <hkzhao123@gmail.com>
2023-03-12 23:30:57 +0800

    add

--- internal/parse/graph.go
+++ internal/parse/graph.go
@@ -1,1 +1,28 @@
 package parse
+
+type OperatorNode struct {
+	params   []string
+	index    int64
+	outType  string
+	funcName string
+	name     string
+}
+
+type GraphNode struct {
+	meta     *OperatorNode
+	name     string
+	funcName string
+	next     map[string]*GraphNode
+	prev     map[string]*GraphNode
+	end      bool
+}
+
+func NewGraph(config []*OperatorNode) *GraphNode {
+	root := &GraphNode{}
+	root.init(config)
+	return root
+}
+
+func (g *GraphNode) init(config []*OperatorNode) {
+	var 
+}
--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -15,16 +15,10 @@ type Parse struct {
 	version    int
 	tensorLen  int
 	tensorType string
+	root       *GraphNode
 }
 
-type OperatorNode struct {
-	in      []string
-	out     string
-	index   int64
-	outType string
-}
-
-func (p *Parse) Parse(config string) {
+func (p *Parse) Parse(config string) error {
 	line := strings.Split(config, ";")
 	checkLine := []string{}
 	for i := 0; i < len(line); i++ {
@@ -33,8 +27,18 @@ func (p *Parse) Parse(config string) {
 		}
 		checkLine = append(checkLine, line[i])
 	}
-	p.check(checkLine)
-
+	if err := p.check(checkLine); err != nil {
+		return err
+	}
+	nodes := []*OperatorNode{}
+	for i := 1; i < len(checkLine); i++ {
+		node, err := p.parseLine(checkLine[i])
+		if err != nil {
+			return err
+		}
+		nodes = append(nodes, node)
+	}
+	return nil
 }
 
 // check: 检查第一行的元信息
@@ -79,4 +83,63 @@ func (p *Parse) check(line []string) error {
 		return errors.New("line1 should contain version,tf_vector_len and type")
 	}
 	return nil
+}
+
+// parseLine: 解析每一行使用的算子和输入输出和具体对应的索引
+// name=tmpName,op=random(jsonPath),type=int64,index=1;
+func (p *Parse) parseLine(line string) (*OperatorNode, error) {
+	ll := strings.Split(line, ",")
+	if len(ll) != 4 {
+		return nil, errors.New("line shouble like name=tmpName,op=random(\"jsonPath\"),type=int64,index=1;")
+	}
+	res := &OperatorNode{}
+	for i := 0; i < len(ll); i++ {
+		if strings.HasPrefix(ll[i], "name") {
+			n := strings.Split(ll[i], "=")
+			if len(n) != 2 {
+				return nil, errors.New("line should be like name=tmpName")
+			}
+			res.name = n[1]
+		} else if strings.HasPrefix(ll[i], "op") {
+			n := strings.Split(ll[i], "=")
+			if len(n) != 2 {
+				return nil, errors.New("line should be like op=random(jsonPath)")
+			}
+			funcIdx := strings.Index(n[1], "(")
+			if funcIdx == -1 {
+				return nil, errors.New("line should be like op=random(jsonPath)")
+			}
+			res.funcName = n[1][:funcIdx]
+			if funcIdx+1 > len(n[1])-1 {
+				return nil, errors.New("line shouble like op=random(jsonPath)")
+			}
+			paramsLine := n[1][funcIdx+1 : len(n[1])-1]
+			if len(paramsLine) == 0 {
+				continue
+			}
+			params := strings.Split(paramsLine, ",")
+			res.params = params
+		} else if strings.HasPrefix(ll[i], "type") {
+			n := strings.Split(ll[i], "=")
+			if len(n) != 2 {
+				return nil, errors.New("line should be type=int64")
+			}
+			res.outType = n[1]
+		} else if strings.HasPrefix(ll[i], "index") {
+			n := strings.Split(ll[i], "=")
+			if len(n) != 2 {
+				return nil, errors.New("line should be index=1")
+			}
+			idx, err := strconv.Atoi(n[1])
+			if err != nil {
+				return nil, errors.New("line should be index=1, index is number")
+			}
+			res.index = int64(idx)
+		}
+	}
+	return res, nil
+}
+
+func (p *Parse) generateGraph(op *OperatorNode) {
+
 }
