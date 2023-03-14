8d50387392ebc3ba4fc4d0c47220c766c8eaafd2
haokai <hkzhao123@gmail.com>
2023-03-15 00:06:20 +0800

    add

--- internal/parse/graph.go
+++ internal/parse/graph.go
@@ -9,20 +9,62 @@ type OperatorNode struct {
 }
 
 type GraphNode struct {
-	meta     *OperatorNode
-	name     string
-	funcName string
-	next     map[string]*GraphNode
-	prev     map[string]*GraphNode
-	end      bool
+	meta       *OperatorNode
+	name       string
+	next       map[string]*GraphNode
+	dependence []string
+	end        bool
 }
 
+// NewGraph: create graph
 func NewGraph(config []*OperatorNode) *GraphNode {
 	root := &GraphNode{}
 	root.init(config)
 	return root
 }
 
+// init: init
 func (g *GraphNode) init(config []*OperatorNode) {
-	var 
+	dig := map[string]int{}
+	out := map[string]struct{}{}
+	om := map[string]*OperatorNode{}
+	state := map[string]*GraphNode{}
+	//需要一个前向节点与后向节点的map
+	pte := map[string][]string{}
+	for i := 0; i < len(config); i++ {
+		om[config[i].name] = config[i]
+		dig[config[i].name] += len(config[i].params)
+		for j := 0; j < len(config[i].params); j++ {
+			pte[config[i].params[j]] = append(pte[config[i].params[j]], config[i].name)
+		}
+	}
+	for k := range om {
+		if _, ok := pte[k]; !ok {
+			out[k] = struct{}{}
+		}
+	}
+	for len(dig) > 0 {
+		for k, v := range dig {
+			if v != 0 {
+				continue
+			}
+			node := &GraphNode{meta: om[k], name: k, dependence: om[k].params, next: map[string]*GraphNode{}, end: false}
+			if _, ok := out[node.name]; ok {
+				node.end = true
+			}
+			if len(om[k].params) == 0 {
+				g.next[k] = node
+			} else {
+				for j := 0; j < len(om[k].params); j++ {
+					state[om[k].params[j]].next[k] = node
+				}
+
+			}
+			for j := 0; j < len(pte[k]); j++ {
+				dig[pte[k][j]]--
+			}
+			state[k] = node
+			delete(dig, k)
+		}
+	}
 }
